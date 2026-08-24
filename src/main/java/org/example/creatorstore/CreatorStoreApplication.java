4d02fa1c7fb7f928125886e7cd1216b17e73c95f
aryanpasi3805 <aryanpasi38@gmail.com>
2026-08-24 19:51:04 +0530

    springg

---
+++ src/main/java/org/example/creatorstore/CreatorStoreApplication.java
@@ -0,0 +1,20 @@
+package org.example.creatorstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorStoreApplication {
+
+    public static void main(String[] args) {
+        // configure of dotenv
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach((entry) -> System.setProperty(
+                entry.getKey(), entry.getValue()
+        ));
+        SpringApplication.run(CreatorStoreApplication.class, args);
+    }
+
+}
