d66b6721723e8c3f08d8ac9c8635f12857a18a78
AryanMV <aryanpasi38@gmail.com>
2026-09-04 20:49:19 +0530

    databases

---
+++ src/main/java/org/example/creatorstore/entities/Order.java
@@ -0,0 +1,47 @@
+package org.example.creatorstore.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="customer_name", nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_email", nullable = false)
+    private String customerEmail;
+
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private String status;
+
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt=LocalDateTime.now();
+    }
+}
---
+++ src/main/java/org/example/creatorstore/entities/OrderItem.java
@@ -0,0 +1,34 @@
+package org.example.creatorstore.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OrderItem{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price_at_purchase",nullable = false)
+    private BigDecimal priceAtPurchase;
+
+    //TODO: relations
+    @ManyToOne
+    @JoinColumn(name = "order_id",nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "name_id", nullable = false)
+    private Product product;
+}
